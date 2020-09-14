ae1c43fcfd4edb6bdbeb946f3f4e8032857a2488
Simjaeryun <61261788+Simjaeryun@users.noreply.github.com>
2020-09-14 16:25:18 +0900

    0914
    0914

--- js/about.js
+++ js/about.js
@@ -8,7 +8,18 @@
 
 function aboutClick() {
   const aboutCtt = document.querySelectorAll(".about_ctt_text");
+  if (!document.querySelector(".show")) {
+    aboutCtt[0].classList.add("show");
+    aboutCtt[1].classList.add("show");
+    aboutCtt[2].classList.add("show");
+    aboutCtt[3].classList.add("show");
+    aboutCtt[4].classList.add("show");
+    aboutCtt[5].classList.add("show");
+  }
+}
 
+function aboutRemove() {
+  const aboutCtt = document.querySelectorAll(".about_ctt_text");
   if (document.querySelector(".show")) {
     aboutCtt[0].classList.remove("show");
     aboutCtt[1].classList.remove("show");
@@ -16,11 +27,16 @@ function aboutClick() {
     aboutCtt[3].classList.remove("show");
     aboutCtt[4].classList.remove("show");
     aboutCtt[5].classList.remove("show");
-  }else
+  }
 }
 
 function init() {
   navBtn[1].addEventListener("click", aboutClick);
+  navBtn[0].addEventListener("click", aboutRemove);
+  navBtn[2].addEventListener("click", aboutRemove);
+  navBtn[3].addEventListener("click", aboutRemove);
+  navBtn[4].addEventListener("click", aboutRemove);
+  navBtn[5].addEventListener("click", aboutRemove);
 }
 
 init();
